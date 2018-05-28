2c6135e35c7e9b91c0dbd4a04a5ade473c8038b8
DevanXU <devan.xu@foxmail.com>
2018-05-28 17:38:47 +0800

    Add more info about Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => (
   <div>
     <h1>Welcome to the Tornadoes Website!</h1>
+    <h1>Congratulations to get from github.</h1>
   </div>
-)
+);
 
-export default Home
+export default Home;
