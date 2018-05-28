41f3e162ba80191681f58d3c85cf51504148a805
DevanXU <devan.xu@foxmail.com>
2018-05-28 21:56:56 +0800

    add link and route for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
+import {Switch, Route, Link} from "react-router-dom";
 
-const Home = () => (
-  <div>
-    <h1>Welcome to the Tornadoes Website!</h1>
-    <h1>Congratulations to get from github.</h1>
-  </div>
-);
+class Welcome extends Component {
+  render () {
+    return (
+      <div>
+        Welcome!
+      </div>
+    );
+  }
+}
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <Link to='/Home'>Welcome</Link>
+        <Switch>
+          <Route path='/Home' component={Welcome} />
+        </Switch>
+      </div>
+    )
+  }
+}
 
 export default Home;
